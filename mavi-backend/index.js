b1f2148a0a22bc578cd812320f58e252f9e687ee
MaVi-SRE <support@mavisolution.com>
2026-03-23 23:45:59 +0530

    Switched to DATABASE_URL connection

--- mavi-backend/index.js
+++ mavi-backend/index.js
@@ -81,12 +81,9 @@ const regionMap = {
 // -------------------------
 // PostgreSQL Connection
 // -------------------------
+
 const pool = new Pool({
-    user: process.env.DB_USER,
-    host: process.env.DB_HOST,
-    database: process.env.DB_NAME,
-    password: process.env.DB_PASSWORD,
-    port: process.env.DB_PORT,
+    connectionString: process.env.DATABASE_URL,
     ssl: {
         rejectUnauthorized: false
     }
